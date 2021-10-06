b407e4821fd8950cdfa4d65aa2664eefbf58f617
Auri Gabriel Castro de Melo <aurimelo.aluno@unipampa.edu.br>
2021-10-06 15:48:45 -0300

    pequenas mudanças de layout

--- addProduct.php
+++ addProduct.php
@@ -1,36 +1,146 @@
 
 <html>
-<head>
-<title>Adicionar Produto</title>
-</head>
+  <head>
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+      crossorigin="anonymous"
+    />
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
+    <!-- Fim do Bootstrap CSS -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Produtos - Bolicho Alegrete</title>
+  </head>
 <body>
+    <!-- Navbar -->
+    <header
+      class="
+        d-flex
+        flex-wrap
+        align-items-center
+        justify-content-center justify-content-md-between
+        py-3
+        px-4
+        mb-4
+        border-bottom
+      "
+    >
+      <a
+        href="/"
+        class="
+          d-flex
+          align-items-center
+          col-md-3
+          mb-2 mb-md-0
+          text-dark text-decoration-none
+        "
+      >
+        <img src="assets/Logo.svg" alt="Bolicho Alegrete" />
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li>
+          <a href="categories.php" class="nav-link px-2 link-dark"
+            >Departamentos</a
+          >
+        </li>
+      <li><a href="#" class="nav-link px-2 link-dark">Promoções</a></li>
+        <li>
+          <a href="contact.php" class="nav-link px-2 link-dark">Contato</a>
+        </li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <form class="col-6-auto col-lg mb-3 mb-lg-0 me-lg-3">
+          <input
+            type="search"
+            class="form-control"
+            placeholder="Pesquisar..."
+            aria-label="Pesquisar"
+          />
+        </form>
+      </div>
+
+      <div class="social-media">
+        <a href="login.php">
+          <span data_btn_login = "Login">
+            <button class="bi bi-person-circle"></button>
+          </span>
+        </a>
+
+        <a href="https://web.whatsapp.com/" target="_blank">
+        <span data_btn_wpp = "Whatsapp">
+          <button class="bi bi-whatsapp"></button>
+        </span>
+        </a>
+
+        <a href="products-favorite.php" >
+          <span data_btn_favorite = "Meus favoritos">
+            <button class="bi bi-heart-fill"></button>
+          </span>
+        </a>
+
+        <a href="cart.html" >
+          <span data_btn_cart = "Carrinho">
+            <button class="bi bi-cart-fill"></button> 
+          </span>
+        </a> 
+      </div>
+    </header>
+    <!-- Fim do Navbar -->
+
+<div class="container">
 <form action="./productAdded.php" method="post">
  
 <b>Adicionar novo produto</b>
  
-<p>Nome:
-<input type="text" name="name" size="100" value="" />
-</p>
+<div class="mb-3">
+<label for="name" class="form-label">Nome</label>
+<input type="text" class="form-control" name="name" id="name"/>
+</div>
  
-<p>Descrição:
-<input type="text" name="description" size="400" value="" />
-</p>
+<div class="mb-3">
+<label for="description" class="form-label">Descrição</label>
+<input type="text" class="form-control" name="description" id="description"/>
+</div>
  
-<p>Imagem:
-<input type="text" name="img" size="400" value="" />
-</p>
+<div class="mb-3">
+<label for="img" class="form-label">Imagem</label>
+<input type="text" class="form-control" name="img" id="img"/>
+</div>
 
-<p>Quantidade:
-<input type="number" name="qtd" size="30" value="" />
-</p>
+<div class="mb-3">
+<label for="qtd" class="form-label">Quantidade</label>
+<input type="number" class="form-control" name="qtd" id="qtd"/>
+</div>
  
-<p>Preço:
-<input type="number" name="price" size="30" value="" />
-</p>
+<div class="mb-3">
+<label for="price" class="form-label">Preço</label>
+<div class="input-group">
+<span class="input-group-text" id="moeda">R$</span>
+<input type="number"class="form-control"  name="price" id="price" aria-label="Preço" aria-describedby="moeda"/>
+</div>
+</div>
  
 <p>
-<input type="submit" name="submit" value="Send" />
+<button class="btn btn-primary" type="submit" name="submit" value="Send">Adicionar</button>
 </p>
 </form>
+</div>
+
+    <!-- Bootstrap JS -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+      crossorigin="anonymous"
+    ></script>
+    <!-- Fim do Bootstrap JS -->
 </body>
 </html>
